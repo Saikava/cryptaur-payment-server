f64e17071bbdf32593382b51a94ef21bcb893bee
blashyrkh <blashyrkh@inbox.ru>
2017-11-16 10:08:40 +0300

    Notification function
    Nonce is automatically added to the notification object. The object is
    serialized as json and signed with HMAC-SHA256. The signature is added
    to the POST request as a custom header field.

---
+++ bin/notify.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import io, binascii, pycurl, json, hashlib, hmac, time
+import config
+
+url=config.config["notify-url"]
+key=config.config["notify-key"]
+
+
+def notify(**args):
+    args["nonce"]=int(time.time()*1000)
+    data=json.dumps(args, sort_keys=True)
+    digest=hmac.new(binascii.a2b_hex(key), data.encode("latin1"), hashlib.sha256).digest()
+
+    buf=io.BytesIO()
+
+    c=pycurl.Curl()
+    c.setopt(pycurl.URL, url)
+    c.setopt(pycurl.HTTPHEADER, ["Content-Type: application/json", "Accept: application/json", "HMAC-Signature: {0}".format(binascii.b2a_hex(digest))])
+    c.setopt(pycurl.POST, 1)
+    c.setopt(pycurl.POSTFIELDS, data)
+    c.setopt(pycurl.IPRESOLVE, 1)
+    c.setopt(pycurl.WRITEFUNCTION, buf.write)
+    c.setopt(pycurl.TIMEOUT, 30)
+    c.setopt(pycurl.CONNECTTIMEOUT, 10)
+    c.perform()
+    c.close()
+
