7cb61fca917e58d63234f00416e153453268a2bb
blashyrkh <blashyrkh@inbox.ru>
2017-11-18 22:20:19 +0300

    Doge deposit handling
    Use listsinceblock wallet RPC command. All unaccepted transactions are
    stored into database to notify again later (probably, confirmations
    number was too low).

--- bin/database.py
+++ bin/database.py
@@ -14,7 +14,34 @@ db.cursor().execute("""CREATE TABLE IF NOT EXISTS depositAddresses
   notified BOOLEAN NOT NULL,
   address TEXT(255),
   PRIMARY KEY(coinname(16), userid)
-)""")
+);
+
+CREATE TABLE IF NOT EXISTS addressToUserID
+(
+  coinname TEXT(16) NOT NULL,
+  address TEXT(255) NOT NULL,
+  userid INT NOT NULL,
+  PRIMARY KEY(coinname(16), address(255))
+);
+
+CREATE TABLE IF NOT EXISTS lastCheckedBlockHeight
+(
+  coinname TEXT(16) NOT NULL,
+  blockHeight INT,
+  PRIMARY KEY(coinname(16))
+);
+
+CREATE TABLE IF NOT EXISTS unacceptedTransactions
+(
+  coinname TEXT(16) NOT NULL,
+  txhash TEXT(255) NOT NULL,
+  vout INT NOT NULL,
+  amount TEXT(100) NOT NULL,
+  blockHeight INT,
+  userid INT NOT NULL,
+  PRIMARY KEY(coinname(16), txhash(255), vout)
+);
+""")
 
 class DepositAddresses:
     def __init__(__self__, coinname):
@@ -36,6 +63,13 @@ class DepositAddresses:
             else:
                 return address
 
+    def getUseridByAddress(__self__, address):
+        global db
+
+        cur=db.cursor()
+        n=cur.execute("SELECT userid FROM addressToUserID WHERE coinname=%s AND address=%s LIMIT 0, 1;", (__self__.coinname, address))
+        return None if n==0 else cur.fetchone()[0]
+
     def listPendingRequests(__self__, maxRequests):
         global db
 
@@ -47,6 +81,7 @@ class DepositAddresses:
         global db
 
         db.cursor().execute("UPDATE depositAddresses SET pending=0, notified=0, address=%s WHERE coinname=%s AND userid=%s;", (address, __self__.coinname, userid))
+        db.cursor().execute("INSERT INTO addressToUserID (coinname, address, userid) VALUES (%s, %s, %s);", (__self__.coinname, address, userid))
         db.commit()
 
     def listUnnotifiedRequests(__self__, maxRequests):
@@ -58,4 +93,35 @@ class DepositAddresses:
 
     def markAsNotified(__self__, userid):
         db.cursor().execute("UPDATE depositAddresses SET notified=1 WHERE coinname=%s AND userid=%s;", (__self__.coinname, userid))
+        db.commit()
+
+
+class Deposits:
+    def __init__(__self__, coinname):
+        __self__.coinname=coinname
+
+    def getLastCheckedBlockHeight(__self__):
+        global db
+
+        cur=db.cursor()
+        n=cur.execute("SELECT blockHeight FROM lastCheckedBlockHeight WHERE coinname=%s;", (__self__.coinname, ))
+        if n==0:
+            return None
+        else:
+            return cur.fetchone()[0]
+
+    def listUnacceptedDeposits(__self__):
+        global db
+
+        cur=db.cursor()
+        cur.execute("SELECT txhash, vout, userid, amount, blockHeight FROM unacceptedTransactions WHERE coinname=%s;", (__self__.coinname, ))
+        return {(txhash, vout):(userid, amount, blockHeight) for txhash, vout, userid, amount, blockHeight in cur.fetchall()}
+
+    def setLastCheckedBlockHeight(__self__, blockHeight, unacceptedTransactions):
+        global db
+
+        db.cursor().execute("INSERT INTO lastCheckedBlockHeight VALUES (%s, %s) ON DUPLICATE KEY UPDATE blockHeight=%s;", (__self__.coinname, blockHeight, blockHeight))
+        db.cursor().execute("DELETE FROM unacceptedTransactions WHERE coinname=%s;", (__self__.coinname,))
+        for (txid,vout),(userid,amount,blockHeight) in unacceptedTransactions.items():
+            db.cursor().execute("INSERT INTO unacceptedTransactions VALUES (%s, %s, %s, %s, %s, %s);", (__self__.coinname, txid, vout, amount, blockHeight, userid))
         db.commit()
--- bin/service-doge.py
+++ bin/service-doge.py
@@ -4,19 +4,110 @@ import json, jsonrpclib
 import config, database, notify
 
 coinname="doge"
-db=database.DepositAddresses(coinname)
+trustWalletAccounts=False
+
+def depositNotify(txid, vout, userid, amount, conf):
+    global coinname
+
+    print("Notify deposit {0}-{1} {2} {3} for user {4} with {5} confirmations".format(txid, vout, amount, coinname.upper(), userid, conf))
+    if notify.notify(coin=coinname.upper(), txid=txid, vout=vout, userid=userid, amount=amount, conf=conf):
+        print("> Accepted!")
+        return True
+    else:
+        print("> Rejected!")
+        return False
+
+def processDepositAddressRequests():
+    global walletrpc
+
+    dbda=database.DepositAddresses(coinname)
+    for userid in dbda.listPendingRequests(100):
+        address=walletrpc.getnewaddress(str(userid))
+        dbda.storeAddress(userid, address)
+        print("Generated {0} deposit address {1} for user {2}".format(coinname.upper(), address, userid))
+
+    for userid,address in dbda.listUnnotifiedRequests(100):
+        print("Notify {0} deposit address {1} for user {2}".format(coinname.upper(), address, userid))
+        if notify.notify(coin=coinname, userid=userid, address=address):
+            dbda.markAsNotified(userid)
+            print("> Accepted!")
+
+def processIncomingDeposits():
+    global walletrpc
+
+    dbd=database.Deposits(coinname)
+    dbda=database.DepositAddresses(coinname)
+
+    lastCheckedBlock=dbd.getLastCheckedBlockHeight()
+
+    if lastCheckedBlock is None:
+        res=walletrpc.listsinceblock("", 1)
+    else:
+        res=walletrpc.listsinceblock(walletrpc.getblockhash(lastCheckedBlock), 1)
+
+    topBlockHash=res["lastblock"]
+    topBlockHeight=walletrpc.getblock(topBlockHash, True)["height"]
+
+    new_txlist={}
+    for tx in res["transactions"]:
+        # It's not an incoming transaction
+        if tx["category"]!="receive":
+            continue
+
+        # Extract account id - either directly from the transaction or indirectly
+        # (use our database to find userid by receiving address)
+        if trustWalletAccounts:
+            try:
+                account=int(tx["account"])
+            except:
+                continue
+        else:
+            account=dbda.getUseridByAddress(tx["address"])
+            if account is None:
+                continue
+
+        # block height of transaction
+        conf=tx["confirmations"]
+        if conf==0:
+            new_txlist[(tx["txid"],tx["vout"])]=(account,str(tx["amount"]),None)
+        else:
+            new_txlist[(tx["txid"],tx["vout"])]=(account,str(tx["amount"]),topBlockHeight-conf+1)
+
+    old_txlist=dbd.listUnacceptedDeposits()
+    new_unacceptedList={}
+
+    for (txid,vout),tx in new_txlist.items():
+        userid,amount,blockHeight=tx
+
+        conf=0
+        if blockHeight is not None:
+            conf=topBlockHeight-blockHeight+1
+
+        if (txid,vout) not in old_txlist:
+            if not depositNotify(txid, vout, userid, amount, conf):
+                new_unacceptedList[(txid,vout)]=tx
+        else:
+            if not depositNotify(txid, vout, userid, amount, conf):
+                new_unacceptedList[(txid,vout)]=tx
+            del old_txlist[(txid,vout)]
+
+    for (txid,vout),tx in old_txlist.items():
+        userid,amount,blockHeight=tx
+
+        conf=0
+        if blockHeight is not None:
+            conf=topBlockHeight-blockHeight+1
+
+        # TODO: check transaction because it may have ceased to exist
+
+        if not depositNotify(txid, vout, userid, amount, conf):
+            new_unacceptedList[(txid,vout)]=tx
+
+    dbd.setLastCheckedBlockHeight(topBlockHeight, new_unacceptedList)
+
 
 rpcparams=config.config["coins"][coinname]
 walletrpc=jsonrpclib.jsonrpc.Server("http://{0}:{1}@{2}:{3}".format(rpcparams["user"], rpcparams["password"], rpcparams["host"], rpcparams["port"]))
 
-
-for userid in db.listPendingRequests(100):
-    address=walletrpc.getnewaddress(str(userid))
-    db.storeAddress(userid, address)
-    print("Generated {0} deposit address {1} for user {2}".format(coinname.upper(), address, userid))
-
-for userid,address in db.listUnnotifiedRequests(100):
-    print("Notify {0} deposit address {1} for user {2}".format(coinname.upper(), address, userid))
-    if notify.notify(coin=coinname, userid=userid, address=address):
-        db.markAsNotified(userid)
-        print("> Accepted!")
+processDepositAddressRequests()
+processIncomingDeposits()
