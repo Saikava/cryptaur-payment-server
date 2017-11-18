b9d01e9b9ddb1756a40c4c21fff844173c3421f2
blashyrkh <blashyrkh@inbox.ru>
2017-11-18 22:33:37 +0300

    ETH/ETC deposit contract
    Smart contract for handling ETH/ETC deposits. Host contract is used to
    create deposit contracts. Deposit contract forwards all incoming
    deposits to the host. Host emits event and forwards money to the cold
    wallet.

---
+++ bin/ethdeposit.sol
@@ -0,0 +1,103 @@
+// blashyrkh.maniac.coding
+
+pragma solidity ^0.4.0;
+
+contract Owned
+{
+    address public owner;
+    address public newOwner;
+
+    function Owned()
+    {
+        owner=msg.sender;
+    }
+
+    modifier onlyOwner
+    {
+        require(owner==msg.sender);
+        _;
+    }
+
+    function changeOwner(address _newOwner) public onlyOwner
+    {
+        require(_newOwner!=0);
+        newOwner=_newOwner;
+    }
+
+    function acceptOwnership() public
+    {
+        require(msg.sender==newOwner);
+        owner=newOwner;
+        delete newOwner;
+    }
+}
+
+contract AbstractDepositHost
+{
+    uint32 public gasAmount;
+    function forwardDeposit(uint32 _userid) public payable;
+}
+
+contract DepositWallet
+{
+    address public host;
+    uint32 public userid;
+
+    function DepositWallet(address _host, uint32 _userid)
+    {
+        require(_host!=0);
+
+        host=_host;
+        userid=_userid;
+    }
+
+    function() payable
+    {
+        uint32 gasAmount=AbstractDepositHost(host).gasAmount();
+        AbstractDepositHost(host).forwardDeposit.value(this.balance).gas(gasAmount)(userid);
+    }
+}
+
+contract DepositHost is Owned, AbstractDepositHost
+{
+    address public forwardAddress;
+    mapping (uint32 => address) public depositAddresses;
+
+    event Deposit(uint32 userid, uint amountWei);
+    event NewDepositAddress(uint32 userid, address depositAddress);
+
+    function DepositHost() Owned()
+    {
+        gasAmount=3000000;
+    }
+
+    function setForwardAddress(address _forwardAddress) public onlyOwner
+    {
+        forwardAddress=_forwardAddress;
+    }
+
+    function setGasAmount(uint32 _newGasAmount) public onlyOwner
+    {
+        gasAmount=_newGasAmount;
+    }
+
+    function forwardDeposit(uint32 _userid) public payable
+    {
+        require(forwardAddress.call.gas(gasAmount).value(this.balance)());
+        Deposit(_userid, msg.value);
+    }
+
+    function getExistingDepositAddress(uint32 _userid) public constant returns(address)
+    {
+        return depositAddresses[_userid];
+    }
+
+    function generateDepositAddress(uint32 _userid) public onlyOwner
+    {
+        if(depositAddresses[_userid]==0)
+        {
+            depositAddresses[_userid]=new DepositWallet(this, _userid);
+        }
+        NewDepositAddress(_userid, depositAddresses[_userid]);
+    }
+}
