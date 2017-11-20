4fc738164c1cf110c08e988625f26dba11da0fe0
blashyrkh <blashyrkh@inbox.ru>
2017-11-20 21:14:06 +0300

    Notification failure doesn't crash the script
    If the notification target is not accessible, notification just fails.
    The exception doesn't crash the script.

--- bin/notify.py
+++ bin/notify.py
@@ -15,19 +15,19 @@ def notify(**args):
 
     buf=io.BytesIO()
 
-    c=pycurl.Curl()
-    c.setopt(pycurl.URL, url)
-    c.setopt(pycurl.HTTPHEADER, ["Content-Type: application/json", "Accept: application/json", "HMAC-Signature: {0}".format(binascii.b2a_hex(digest))])
-    c.setopt(pycurl.POST, 1)
-    c.setopt(pycurl.POSTFIELDS, data)
-    c.setopt(pycurl.IPRESOLVE, 1)
-    c.setopt(pycurl.WRITEFUNCTION, buf.write)
-    c.setopt(pycurl.TIMEOUT, 30)
-    c.setopt(pycurl.CONNECTTIMEOUT, 10)
-    c.perform()
-    c.close()
-
     try:
+        c=pycurl.Curl()
+        c.setopt(pycurl.URL, url)
+        c.setopt(pycurl.HTTPHEADER, ["Content-Type: application/json", "Accept: application/json", "HMAC-Signature: {0}".format(binascii.b2a_hex(digest))])
+        c.setopt(pycurl.POST, 1)
+        c.setopt(pycurl.POSTFIELDS, data)
+        c.setopt(pycurl.IPRESOLVE, 1)
+        c.setopt(pycurl.WRITEFUNCTION, buf.write)
+        c.setopt(pycurl.TIMEOUT, 30)
+        c.setopt(pycurl.CONNECTTIMEOUT, 10)
+        c.perform()
+        c.close()
+
         return json.loads(buf.getvalue())["accept"]
     except:
         return False
